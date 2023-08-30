e2e368166b665552a956d2eb1e68b59b9f82c262
CuongVnZ <edgcuong@gmail.com>
2023-08-30 08:09:17 +0700

    Update

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -24,18 +24,7 @@ export default function Product() {
   
     const MONTHS = useMemo(
       () => [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Agu",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec",
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
       ],
       []
     );
@@ -202,126 +191,125 @@ export default function Product() {
     }
 
     return (
-        <div className="wrapper">
-            <div className="productTitleContainer">
-                <h1 className="productTitle">Product</h1>
-                <a href={"http://ilc-client.vercel.app/product/" + product._id}>
-                  <button className="productViewButton">View</button>
-                </a>
-            </div>
-            <div className="productTop">
-                <div className="productTopLeft">
-                    <Chart data={pStats} dataKey="Sales" title="Sales Performance"/>
-                </div>
-                <div className="productTopRight">
-                    <div className="productInfoTop">
-                        <img src={product.img} alt="" className="productInfoImg" />
-                        <span className="productName">{product.title}</span>
-                    </div>
-                    <div className="productInfoBottom">
-                        <div className="productInfoItem">
-                            <span className="productInfoKey">Database id:</span>
-                            <span className="productInfoValue">{product._id}</span>
-                        </div>
-                        <div className="productInfoItem">
-                            <span className="productInfoKey">Product id:</span>
-                            <span className="productInfoValue">{product.pid}</span>
-                        </div>
-                        <div className="productInfoItem">
-                            <span className="productInfoKey">Description:</span>
-                            <span className="productInfoValue">{product.desc}</span>
-                        </div>
-                        <div className="productInfoItem">
-                            <span className="productInfoKey">Sales:</span>
-                            <span className="productInfoValue">100</span>
-                        </div>
-                        <div className="productInfoItem">
-                            <span className="productInfoKey">Price:</span>
-                            <span className="productInfoValue">{product.price}</span>
-                        </div>
-                        <div className="productInfoItem">
-                            <span className="productInfoKey">In stock:</span>
-                            <span className="productInfoValue">{String(product.inStock)}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="productBottom">
-                <form className="productForm" onSubmit={handleSubmit}>
-                <div className="productFormTop">
-                    <div className="productFormTopLeft">
-                        <label>Product Name</label>
-                        <input name="title" type="text" placeholder={product.title} defaultValue={product.title} onChange={handleChange}/>
-                        <br></br>
-
-                        <label>Product id</label>
-                        <input name="pid" type="text" placeholder={product.pid} defaultValue={product.pid} onChange={handleChange}/>
-                        <br></br>
-
-                        <label>Product Description</label>
-                        <textarea rows="4" cols="50" name="desc" placeholder={product.desc} defaultValue={product.desc} onChange={handleChange}/>
-                        <br></br>
-
-                        <label>Product Category</label>
-                        <input name="category" type="text" placeholder={product.category} defaultValue={product.category} onChange={handleChange}/>
-                        <br></br>
-                        
-                        <label>Product Pice</label>
-                        <input name="price" type="text" placeholder={product.price} defaultValue={product.price} onChange={handleChange}/>
-                        <br></br>
-
-                        <label>Product Types</label>
-                        <button type="button" onClick={addType}>Add Type</button>
-                        {types.map((field, index) => (
-                          <div key={index}>
-                            <input type="text" defaultValue={field.name} name='name' onChange={handleChangeType(index)} />
-                            <select name="mode" defaultValue={field.mode} onChange={handleChangeType(index)}>
-                              <option value="add">Add</option>
-                              <option value="multiply">Multiply</option>
-                            </select>
-                            <input type="text" defaultValue={field.price} name='price' onChange={handleChangeType(index)} />
-                            <button type="button" onClick={() => removeType(index)}>Remove</button>
-                          </div>
-                        ))}
-                        <br></br>
-
-                        <label>Product Options</label>
-                        <button type="button" onClick={addOption}>Add Option</button>
-                        {options.map((field, index) => (
-                          <div key={index}>
-                            <input type="text" defaultValue={field.name} name='name' onChange={handleChangeOption(index)} />
-                            <select name="mode" defaultValue={field.mode} onChange={handleChangeOption(index)}>
-                              <option value="add">Add</option>
-                              <option value="multiply">Multiply</option>
-                            </select>
-                            <input type="text" defaultValue={field.price} name='price' onChange={handleChangeOption(index)} />
-                            <button type="button" onClick={() => removeOption(index)}>Remove</button>
-                          </div>
-                        ))}
-                        <br></br>
-
-                        
-                        <label>In Stock</label>
-                        <select name="inStock" onChange={handleChange}>
-                            <option value="true">Yes</option>
-                            <option value="false">No</option>
-                        </select>
-                    </div>
-                    <div className="productFormTopRight">
-                        <div className="productUpload">
-                            <img src={product.img} alt="" className="productUploadImg" />
-                            <label htmlFor="file">
-                                <Publish/>
-                            </label>
-                            <input type="file" id="file" style={{display:"none"}} onChange={(e) => setFile(e.target.files[0])}/>
-                        </div>
-                    </div>
-                </div>
-                <div className="productFormBottom">
-                    <button type="submit" className="productButton">Update</button>
-                </div>
-                </form>
-            </div>
+      <div className="wrapper">
+        <div className="productTitleContainer">
+          <h1 className="productTitle">Product</h1>
+          <a href={"http://ilc-client.vercel.app/product/" + product._id}>
+            <button className="productViewButton">View</button>
+          </a>
         </div>
+        <div className="productTop">
+          <div className="productTopLeft">
+            <Chart data={pStats} dataKey="Sales" title="Sales Performance"/>
+          </div>
+          <div className="productTopRight">
+              <div className="productInfoTop">
+                <img src={product.img} alt="" className="productInfoImg" />
+                <span className="productName">{product.title}</span>
+              </div>
+              <div className="productInfoBottom">
+                <div className="productInfoItem">
+                  <span className="productInfoKey">Database id:</span>
+                  <span className="productInfoValue">{product._id}</span>
+                </div>
+                <div className="productInfoItem">
+                  <span className="productInfoKey">Product id:</span>
+                  <span className="productInfoValue">{product.pid}</span>
+                </div>
+                <div className="productInfoItem">
+                  <span className="productInfoKey">Description:</span>
+                  <span className="productInfoValue">{product.desc}</span>
+                </div>
+                <div className="productInfoItem">
+                  <span className="productInfoKey">Sales:</span>
+                  <span className="productInfoValue">100</span>
+                </div>
+                <div className="productInfoItem">
+                  <span className="productInfoKey">Price:</span>
+                  <span className="productInfoValue">{product.price}</span>
+                </div>
+                <div className="productInfoItem">
+                  <span className="productInfoKey">In stock:</span>
+                  <span className="productInfoValue">{String(product.inStock)}</span>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div className="productBottom">
+          <form className="productForm" onSubmit={handleSubmit}>
+            <div className="productFormTop">
+              <div className="productFormTopLeft">
+                <label>Product Name</label>
+                <input name="title" type="text" placeholder={product.title} defaultValue={product.title} onChange={handleChange}/>
+                <br></br>
+
+                <label>Product id</label>
+                <input name="pid" type="text" placeholder={product.pid} defaultValue={product.pid} onChange={handleChange}/>
+                <br></br>
+
+                <label>Product Description</label>
+                <textarea rows="4" cols="50" name="desc" placeholder={product.desc} defaultValue={product.desc} onChange={handleChange}/>
+                <br></br>
+
+                <label>Product Category</label>
+                <input name="category" type="text" placeholder={product.category} defaultValue={product.category} onChange={handleChange}/>
+                <br></br>
+                
+                <label>Product Pice</label>
+                <input name="price" type="text" placeholder={product.price} defaultValue={product.price} onChange={handleChange}/>
+                <br></br>
+
+                <label>Product Types</label>
+                <button type="button" onClick={addType}>Add Type</button>
+                {types.map((field, index) => (
+                  <div key={index}>
+                    <input type="text" defaultValue={field.name} name='name' onChange={handleChangeType(index)} />
+                    <select name="mode" defaultValue={field.mode} onChange={handleChangeType(index)}>
+                      <option value="add">Add</option>
+                      <option value="multiply">Multiply</option>
+                    </select>
+                    <input type="text" defaultValue={field.price} name='price' onChange={handleChangeType(index)} />
+                    <button type="button" onClick={() => removeType(index)}>Remove</button>
+                  </div>
+                ))}
+                <br></br>
+
+                <label>Product Options</label>
+                <button type="button" onClick={addOption}>Add Option</button>
+                {options.map((field, index) => (
+                  <div key={index}>
+                    <input type="text" defaultValue={field.name} name='name' onChange={handleChangeOption(index)} />
+                    <select name="mode" defaultValue={field.mode} onChange={handleChangeOption(index)}>
+                      <option value="add">Add</option>
+                      <option value="multiply">Multiply</option>
+                    </select>
+                    <input type="text" defaultValue={field.price} name='price' onChange={handleChangeOption(index)} />
+                    <button type="button" onClick={() => removeOption(index)}>Remove</button>
+                  </div>
+                ))}
+                <br></br>
+
+                <label>In Stock</label>
+                <select name="inStock" onChange={handleChange}>
+                  <option value="true">Yes</option>
+                  <option value="false">No</option>
+                </select>
+              </div>
+              <div className="productFormTopRight">
+                <div className="productUpload">
+                  <img src={product.img} alt="" className="productUploadImg" />
+                  <label htmlFor="file">
+                    <Publish/>
+                  </label>
+                  <input type="file" id="file" style={{display:"none"}} onChange={(e) => setFile(e.target.files[0])}/>
+                </div>
+              </div>
+            </div>
+            <div className="productFormBottom">
+                <button type="submit" className="productButton">Update</button>
+            </div>
+          </form>
+        </div>
+      </div>
     );
 }
